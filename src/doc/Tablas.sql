85b29b086524ebbbd08bc6a5316d760751e33d1f
Jose David Suarez Cardona <josedavid4227@gmail.com>
2022-08-02 09:59:16 -0500

    implementacion de la base de datos

--- src/doc/Tablas.sql
+++ src/doc/Tablas.sql
@@ -5,7 +5,7 @@ Create table Mesas(
 );
 
 Create table Adicional(
-    id integer primary key
+    id integer primary key,
     nombre varchar(100) not null,
     precio integer not null
 
@@ -17,14 +17,14 @@ create table Pedidos (
     estado varchar(50) not null,
     id_mesa integer not null,
     id_adicional integer not null,
-    constraint id_mesa_fk foreign key (id_mesa) references Mesa(id)
+    constraint id_mesa_fk foreign key (id_mesa) references Mesas(id)
 );
 
 create table PedidoAdicional(
     id_pedido integer ,
     id_adicional integer ,
     constraint pedidoAdicional_pk primary key (id_adicional,id_pedido),
-    constraint pedido_fk foreign key (id_pedido) references Pedido(id),
+    constraint pedido_fk foreign key (id_pedido) references Pedidos(id),
     constraint adicional_fk foreign key (id_adicional) references Adicional(id)
 
 );
@@ -59,12 +59,14 @@ create table opcionesPrincipio(
 
 create table Corrientazos(
     id integer primary key,
+    id_pedido integer,
     precio integer not null,
     id_carne not null,
     id_sopa not null,
     id_jugo not null,
     id_principio not null,
     id_ensalada not null,
+    constraint id_pedido_fk foreign key (id_pedido) references Pedidos(id),
     constraint id_carne_fk foreign key (id_carne) references opcionesCarne(id),
     constraint id_sopa_fk foreign key (id_sopa) references opcionesSopa(id),
     constraint id_ensalada_fk foreign key (id_ensalada) references opcionesEnsalada(id),
